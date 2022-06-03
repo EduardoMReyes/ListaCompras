d03b7e739976464ac425331f60f1fa99052a8983
EduardoReyes <eduardo.mreyes94@gmail.com>
2022-06-03 12:21:09 -0500

    Se agrega la funcionalidad para almacenar en localStorage los elementos de la tabla y se leen cuando carga la página

--- js/main.js
+++ js/main.js
@@ -1,39 +1,17 @@
     let contador = 0;
     let costoTotal = 0;
     let totalEnProductos = 0;
-    let element = document.getElementById("totalPrecio")
+    //arreglo global para almacenar lista de compras
+    let datos = [];
+
+    let element = document.getElementById("totalPrecio");
     element.innerHTML="Total en precio";
-
     let txtNombre= document.getElementById("Name");
-    // txtNombre.value="Leche Semidescremada";
-    // console.log(txtNombre.value[0]);
     let txtNumber = document.getElementById("Number");
-
     let total = document.getElementById("precioTotal");
-
-    // let campos = document.getElementsByClassName("campo");
-    // campos[0].value = "Leche descremada deslactosada light=Agua";
-    // console.log(campos);
-
-    // for (let i=0; i<campos.length; i++){
-    //     campos[i].style.border="red thin solid";
-    // }//for
-
-    // let spans = document.getElementsByTagName("span");
-    // for (let i=0; i<spans.length; i++) {
-    //     console.log(spans[i].textContent);
-//  }//for i
 
     let tabla = document.getElementById("tablaListaCompras");
     let cuerpoTabla = tabla.getElementsByTagName("tbody");
-
-    // cuerpoTabla[0].innerHTML = `<tr>
-    // <th scope="row">1</th>
-    // <td>Leche Descremada</td>
-    // <td>3</td>
-    // <td>$23.00</td>
-    // </tr>
-    // `;
 
     function validarNombre(){
         if(txtNombre.value.length <= 3 ){
@@ -65,9 +43,9 @@
                 txtNombre.style.border="red thin solid";
                 lista+="<li>Se debe escribir un nombre válido</li>";
             }//if
-            if(!validarCantidad()){
+            if(! validarCantidad() ){
                 txtNumber.style.border="red thin solid";
-                lista+="<li>Se debe escribir una cantidad válida</li>"
+                lista+="<li>Se debe escribir una cantidad válida</li>";
             }//if 
             
             document.getElementById("alertValidacionesTexto").innerHTML=
@@ -92,11 +70,23 @@
         let precio = (Math.floor( (Math.random() * 50)*100))/100;
         let cantidad = parseFloat (txtNumber.value);
         totalEnProductos += Math.ceil(cantidad);
-        document.getElementById("productosTotal").innerHTML = totalEnProductos
+        document.getElementById("productosTotal").innerHTML = totalEnProductos;
         localStorage.setItem ("productosTotal", totalEnProductos);
         costoTotal += (precio * cantidad);
-        total.innerHTML = `$ ${costoTotal.toFixed(2)}`
+        total.innerHTML = `$ ${costoTotal.toFixed(2)}`;
         localStorage.setItem ("precioTotal", costoTotal.toFixed(2));
+        
+        //JSON
+        let elemento = `{ "id": ${contador},
+            "nombre": "${txtNombre.value}",
+            "cantidad": "${txtNumber.value}",
+            "precio": ${precio}
+        }`;
+        
+        datos.push(JSON.parse(elemento));
+        localStorage.setItem("elementosTabla", JSON.stringify(datos));
+        // console.log(datos);
+
         let tmp= `<tr>
         <th scope="row">${contador}</th>
         <td>${txtNombre.value}</td>
@@ -120,20 +110,32 @@ txtNumber.addEventListener("blur", (event)=>{
     }
 );
 
-
-
 window.addEventListener("load", function(){
-    if (localStorage.getItem ("contadorProductos")!=null){
+        if (localStorage.getItem ("contadorProductos") != null){
         contador = parseInt(localStorage.getItem ("contadorProductos"));
         document.getElementById("contadorProductos").innerHTML = contador;
-    }//if contadorProductos
-     if(localStorage.getItem ("productosTotal")){
-        totalEnProductos = parseInt(localStorage.getItem ("productosTotal"));
-        document.getElementById("productosTotal").innerHTML = totalEnProductos;
-     }//if productosTotal
-     if(localStorage.getItem ("precioTotal")){
-        costoTotal = parseInt(localStorage.getItem("precioTotal"));
-        total.innerHTML = costoTotal;
-      }//if precioTotal
+        }//if contadorProductos
+         if(localStorage.getItem ("productosTotal") !=null){
+         totalEnProductos = parseInt(localStorage.getItem ("productosTotal"));
+         document.getElementById("productosTotal").innerHTML = totalEnProductos;
+         }//if productosTotal
+          if(localStorage.getItem ("precioTotal")!=null){
+          costoTotal = parseInt(localStorage.getItem("precioTotal"));
+          total.innerHTML = costoTotal;
+          }//if precioTotal
+    
+
+           if (localStorage.getItem ("elementosTabla") != null){
+                datos=JSON.parse(this.localStorage.getItem("elementosTabla"));
+                datos.forEach(element => {
+                    cuerpoTabla[0].innerHTML += `<tr>
+                    <th scope="row">${element.id}</th>
+                    <td>${element.nombre}</td>
+                    <td>${element.cantidad}</td>
+                    <td>$ ${element.precio}</td>
+                    </tr>`;
+                });
+           }//if elementosTabla
     }
+    
 );
